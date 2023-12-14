8ad65a3686d582c7b7e734a920403b8ae39a29f5
kumaresan2001 <kumaresancrseven@gmail.com>
2023-12-14 12:08:43 +0530

    new

---
+++ src/Global.js
@@ -0,0 +1,2 @@
+export const API = "https://funnelhq-backend.onrender.com";
+// export const API = "http://localhost:5000";`
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Background from "../Background";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="home">welcome to APP</h1>
+      <Background />
+    </>
+  );
+};
+
+export default Home;
